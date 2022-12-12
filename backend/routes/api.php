cdf1f0e48a0008853ca68aad1cd1112cb3162958
luizmarcello <luizmarcello.vmo@hotmail.com>
2022-12-11 22:12:02 -0300

    Londrina:11.12.22_22:11hs

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,8 +12,14 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+|*/
+
+// Para o frontend, este backend será como uma "api", só
+// retornando os dados para este frontend
+// Colocar no frontend: api/users
+Route::get('users', function(){
+    return User::all();
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
